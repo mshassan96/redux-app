074492fc308272d1d959a9d1e2fd7ba6173e0790
mshassan96 <miansalmanhassan@yahoo.com>
2022-09-04 02:59:53 +0500

    Added store

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,12 +1,12 @@
-export default reducer = (state = 0, action) => {
+const reducer = (state = 0, action) => {
   switch (action.type) {
     case "deposit":
       return state + action.payload;
-      break;
     case "withdraw":
       return state - action.payload;
-      break;
     default:
       return state;
   }
 };
+
+export default reducer;
